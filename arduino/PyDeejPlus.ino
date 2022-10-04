bda5dade8edba358299260bc28ea6a6538da5a44
Nina Koninckx <nina.koninckx@outlook.be>
2022-10-04 19:00:36 +0200

    Moved classes around because I didn't understand how executable creation worked, it works now! :P

--- arduino/PyDeejPlus.ino
+++ arduino/PyDeejPlus.ino
@@ -14,10 +14,10 @@ int longPressDelay = 350;           //customizable encoderValues
 int spamSpeed = 15;
 
 int layout[4][4] = {               //layout grid for characters
-  {1,2,3,4},
-  {5,6,7,8},
-  {9,10,11,12},
-  {13,14,15,16},
+  {4,3,2,1},
+  {8,7,6,5},
+  {12,11,10,9},
+  {16,15,14,13},
 };
 int keyDown[4][4];
 bool keyLong[4][4];
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='PyDeejPlus',
+    version='0.0.1',
+    packages=['src', 'src.domain', 'src.services'],
+    url='https://github.com/ShadowSparkyFox/PyDeejPlus',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Nina Koninckx',
+    author_email='nina.koninckx@glassfox.be',
+    description='Deej, but with macro keys and in Python'
+)
